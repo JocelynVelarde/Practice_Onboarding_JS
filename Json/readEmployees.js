fa3ff5b9bfb4b1da1e79bb88697bd08388968fc7
JocelynVelarde <a01285780@itesm.mx>
2023-07-12 22:45:24 -0600

    method to read json file

--- Json/readEmployees.js
+++ Json/readEmployees.js
@@ -1,1 +1,18 @@
-const jsonObjects = require('./employees.json');
+const jsonObject = require('./employees.json');
+
+console.log(jsonObject.Employees.Employee1.Name);
+
+var employees = jsonObject["Employees"];
+
+function readEmployees(json){
+    for(x in json){
+        var object = json[x];
+        if(object.Interests == undefined){
+            console.log(x + " does not have any interest");
+        }else
+        console.log(x + " " + object.Interests);
+    }
+}
+
+readEmployees(employees);
+
