24985aff428898d723e8051bb0908ce32c5ee9c6
JocelynVelarde <a01285780@itesm.mx>
2023-07-13 09:32:16 -0600

    formated document to present

--- Json/recursive.js
+++ Json/recursive.js
@@ -1,17 +1,17 @@
-const jsonObject = require('./employees.json');
+const jsonObject = require("./employees.json");
 
-jsonObject.filter(item => {
-    iterate(item);
+jsonObject.filter((item) => {
+  iterate(item);
 });
 
-function iterate(object){
-    for(x in object){
-        if(typeof(object[x]) == "object"){
-            iterate(object[x]);
-        }else{
-            if(x == "Interests" || x == "Name"){
-                console.log(object[x]);
-            }
-        }
+function iterate(object) {
+  for (x in object) {
+    if (typeof object[x] == "object") {
+      iterate(object[x]);
+    } else {
+      if (x == "Interests" || x == "Name") {
+        console.log(object[x]);
+      }
     }
+  }
 }
