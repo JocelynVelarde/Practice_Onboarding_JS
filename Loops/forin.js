149f9adb9f3a9e8f18bdb8a770c36be102984946
JocelynVelarde <a01285780@tec.mx>
2023-07-12 20:41:50 -0600

    test

--- Loops/forin.js
+++ Loops/forin.js
@@ -1,5 +1,5 @@
 //iterar en un objeto (permite keys y values)
-const person = {name:"John", lname:"Doe", age:24};
+const person = {name:"John", lname:"Doe", age:50};
 
 for (let x in person) {
     let name = person[x];
